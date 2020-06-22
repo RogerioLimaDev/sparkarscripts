4c733bc9f8322e24039e0346b26ca26e85fae081
TropicalCyborg <tropicalcyborg@gmail.com>
2020-06-21 22:00:06 -0300

    Update receive to display each vector individually

--- receive.js
+++ receive.js
@@ -6,9 +6,13 @@ variable.then((message) => {
   const variableReceved = message
   let vectorReceived = variableReceved
   Diagnostics.log('não deu pau até agora: ')
+  const myX = vectorReceived.x.pinLastValue()
+  const myY = vectorReceived.y.pinLastValue()
+  const myZ = vectorReceived.z.pinLastValue()
+  Diagnostics.log('Valor de X: '+myX)
+  Diagnostics.log('Valor de Y: '+myY)
+  Diagnostics.log('Valor de Z: '+myZ)
 
-  const myNumber = vectorReceived.y.pinLastValue()
-  Diagnostics.log(myNumber)
 }).catch(() => {
 
   Diagnostics.log('Fudeu')
